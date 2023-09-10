6edd981ac44e2426235b2bdf4461ed8693e2e8f3
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-09-10 20:45:46 +0530

    Terraform

--- For-each/provider.tf
+++ For-each/provider.tf
@@ -11,3 +11,12 @@ provider "aws" {
   # Configuration options
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "roboshop-remote-state27"
+    key    = "RemoteState-Demo"
+    region = "us-east-1"
+    dynamodb_table = "Roboshop-Lock"
+  }
+}
